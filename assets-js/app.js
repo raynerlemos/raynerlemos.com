7d11c6401ee12833e22dded7f9c03826782fc809
Rayner Lemos <raynerlemos@grm.com.br>
2018-05-30 11:38:20 -0300

    Add plugin particlejs

--- assets-js/app.js
+++ assets-js/app.js
@@ -1,3 +1,9 @@
 $(function(){
 
+    // PLUGIN PARTICLES
+    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+    particlesJS.load('particles-js', 'assets-js/particlesjs-config.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+
 });
